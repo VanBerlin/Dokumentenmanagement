8303bbff00be326feb851e7fdf07a9b680ffe6c8
s546018 <75528737+s546018@users.noreply.github.com>
2024-02-02 14:32:18 +0100

    Initialer Commit

---
+++ src/main/java/com/dokuman/dokumentenmangement/DokumentenmangementApplication.java
@@ -0,0 +1,13 @@
+package com.dokuman.dokumentenmangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokumentenmangementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DokumentenmangementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dokuman/dokumentenmangement/DokumentenmangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dokuman.dokumentenmangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DokumentenmangementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
